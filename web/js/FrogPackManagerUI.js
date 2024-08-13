183e7fa6e6ce41dd4e69e3c8dc208a2e19edcaa1
Seeroy <37861929+Seeroy@users.noreply.github.com>
2024-08-13 10:47:45 +0300

    Изменена логика работы модальных окон

--- web/js/FrogPackManagerUI.js
+++ web/js/FrogPackManagerUI.js
@@ -1,18 +1,19 @@
 let packman__currentMode = "mods";
 let packman__currentModpack = "";
 
-$(function() {
-    $("#modal-packManager .tabs .tab").click(function() {
-        if(!$(this).hasClass("active")) {
+$(function () {
+    $("#modal-packManager .tabs .tab").click(function () {
+        if (!$(this).hasClass("active")) {
             $("#modal-packManager .tabs .tab.active").removeClass("active");
             $(this).addClass("active");
 
             packman__currentMode = $(this).data("tab");
+            FrogPackManagerUI.reloadAll();
         }
     })
 
-    // Установить мод
-    $("#modal-packManager .tabs button.install").click(function() {
+    // Установить из репозитория
+    $("#modal-packManager .tabs button.install").click(function () {
         $("#modal-packs .search").val("");
         FrogPacksUI.refreshDirectorySelect();
         $(`#modal-packs #packs_dirList`).val(packman__currentModpack.id);
@@ -22,12 +23,52 @@ $(function() {
         FrogModals.hideModal("packManager");
         FrogModals.switchModal("packs");
     })
+
+    // Выбрать файл
+    $("#modal-packManager .tabs button.add").click(function () {
+        let properties = {
+            filters: [{name: "Zip file", extensions: ["zip"]}]
+        }
+        if (packman__currentMode === "mods") {
+            properties = {
+                filters: [{name: "Jar file", extensions: ["jar"]}]
+            }
+        }
+        ipcRenderer.invoke("open-dialog", properties).then(result => {
+            if(result === false){
+                return;
+            }
+            let fileName = path.basename(result[0]);
+            if (packman__currentMode !== "worlds") {
+                let fileNewPath = path.join(GAME_DATA, "modpacks", packman__currentModpack.id, packman__currentMode, fileName);
+                if (!fs.existsSync(path.dirname(fileNewPath))) {
+                    fs.mkdirSync(path.dirname(fileNewPath));
+                }
+                fsExtra.moveSync(result[0], fileNewPath);
+                FrogToasts.create(fileName, "download_done", MESSAGES.packManager.installed);
+                return FrogPackManagerUI.reloadAll();
+            } else {
+                let savesDirectory = path.join(GAME_DATA, "modpacks", packman__currentModpack.id, "saves");
+                if (!fs.existsSync(savesDirectory)) {
+                    fs.mkdirSync(savesDirectory);
+                }
+                FrogWorldsManager.installFromZip(savesDirectory, result[0]).then(result => {
+                    if (!result) {
+                        FrogToasts.create(fileName, "error", MESSAGES.packManager.failedWorld);
+                    } else {
+                        FrogToasts.create(fileName, "download_done", MESSAGES.packManager.successWorld);
+                    }
+                    return FrogPackManagerUI.reloadAll();
+                })
+            }
+        })
+    })
 })
 
 class FrogPackManagerUI {
     // Перезагрузить всё
     static reloadAll = () => {
-        if(packman__currentMode === "worlds"){
+        if (packman__currentMode === "worlds") {
             $("#modal-packManager .tabs button.install").hide();
         } else {
             $("#modal-packManager .tabs button.install").show();
@@ -57,13 +98,14 @@ class FrogPackManagerUI {
                 FrogStarter.simpleStart("pack-" + manifest.id);
                 FrogModals.hideModal("packManager");
             })
+            FrogPackManagerUI.reloadAll();
             return resolve(true);
         })
     }
 
     // Загрузить пак и показать окно
     static loadAndShow = (packId) => {
-        if(!FrogPacks.isModpackExists(packId)){
+        if (!FrogPacks.isModpackExists(packId)) {
             return false;
         }
 
